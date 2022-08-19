d4f1ede0081f177d732c6de7c86b9979e465992b
Tomas Monge <tomasmonge@Tomass-MacBook-Pro.local>
2022-08-19 19:03:16 -0300

    fix

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
   
-  app.listen(port, () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening on port ${port}`)
   })
