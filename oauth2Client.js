4d9217a80622daac4401d41d487dbe91f41d36c6
Tomas Monge <tomasmonge@Tomass-MacBook-Pro.local>
2022-12-26 11:58:28 -0300

    fix

--- oauth2Client.js
+++ oauth2Client.js
@@ -2,7 +2,7 @@ import { google } from 'googleapis';
 const CLIENT_ID = '475388183627-8s0qiu7nglrpv5qkg877njn8jar4gpqa.apps.googleusercontent.com'
 const CLIENT_secret = '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'
 const redirect_url = 'https://developers.google.com/oauthplayground'
-const refresh_token = '1//04jDaXjoJvZH7CgYIARAAGAQSNwF-L9IryAb3ZfixR26W58L5_5vx2_WjklPIhM8ucVotdnKJ6F3iFQJ_Wg5j630PFdbKlbGITHc'
+const refresh_token = '1//044lLjEk-4ChnCgYIARAAGAQSNwF-L9IrhSHgFBDnKfTPS6cS1g3Gw_mi-Og6ARZVgQg9NsvWXlPA865H0UonNMadxiRC6REm0Zc'
 
 const oauth2client = new google.auth.OAuth2(
     CLIENT_ID,
