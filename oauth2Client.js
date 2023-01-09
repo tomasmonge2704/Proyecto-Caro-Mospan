8bc94339d0de37dcda2468b1d040eb42cac9bb77
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-01-09 11:13:12 -0300

    fix

--- oauth2Client.js
+++ oauth2Client.js
@@ -2,7 +2,7 @@ import { google } from 'googleapis';
 const CLIENT_ID = '475388183627-8s0qiu7nglrpv5qkg877njn8jar4gpqa.apps.googleusercontent.com'
 const CLIENT_secret = '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'
 const redirect_url = 'https://developers.google.com/oauthplayground'
-const refresh_token = '1//0489jUJ9L3qXCCgYIARAAGAQSNwF-L9Ir-wtQr75vAzd_Zv4vx2lK2ukwTUGf4S8v4vFDSyW0-wUjmI1p1UpSsAOddQWTPggYLMU'
+const refresh_token = '1//04i_l0fUsLQcFCgYIARAAGAQSNwF-L9IrVMocunR4mvmAFSsQHfiItuqqmibSFX_rtuUg1v7dVq_cE4OY8pb1PcMHFTD8BDseAIs'
 
 const oauth2client = new google.auth.OAuth2(
     CLIENT_ID,
