2c209a7ed4904c31678073351c639b049649bedf
Tomas Monge Vidal <87153607+tomasmonge2704@users.noreply.github.com>
2023-05-15 09:21:16 -0300

    Update oauth2Client.js

--- oauth2Client.js
+++ oauth2Client.js
@@ -4,7 +4,7 @@ import { OAuth2Client } from "google-auth-library";
 const CLIENT_ID = '475388183627-8s0qiu7nglrpv5qkg877njn8jar4gpqa.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//044eHopoyHtWJCgYIARAAGAQSNwF-L9IrtyHkQwVnJ70UfcweBXZ2nxz8c3hIZA6UuozieuCqsK38RMJLME8q6VFddRnvmy-03TE';
+const REFRESH_TOKEN = '1//04hDQ7tFWl6emCgYIARAAGAQSNwF-L9IrRRXvZmIT17-PPSK5m_08ODaNG2r4miR6jwk3zG10JZyHVqeYFqd2mH17Ed9jTFG0p5I';
 
 // Crear un objeto OAuth2Client y establecer el token de actualización
 const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
